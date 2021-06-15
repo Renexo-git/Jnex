b0597c629de1da4ff8750941989350e228c795ba
Renexo <contato@renexo.com.br>
2021-06-15 15:57:55 -0300

    Updated

--- modules/global/packages/index.js
+++ modules/global/packages/index.js
@@ -2,15 +2,15 @@ try {
     module.exports = {
         jnex: require("./node_modules/jnex")
     };
-} catch(err) {
+} catch (error) {
     module.exports = {
         jnex: () => ""
     };
-    if (err.code === "MODULE_NOT_FOUND") {
+    if (error.code === "MODULE_NOT_FOUND") {
         console.clear();
         console.log("Installing packages from the global module...");
         const exec = require("child_process").exec;
-        exec("npm install --prefix " + __dirname, (err, stdout, stderr) => {
+        exec("npm install --prefix " + __dirname, (error, stdout, stderr) => {
             console.clear();
             console.log("Done!");
             console.log(stdout);
