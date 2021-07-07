c457dcf90d5880e44fccfe7f09338f805e0412ce
Renexo <contato@renexo.com.br>
2021-07-07 17:09:26 -0300

    Updated

--- modules/global/packages/index.js
+++ modules/global/packages/index.js
@@ -4,16 +4,18 @@ try {
     };
 } catch (error) {
     module.exports = {
-        jnex: () => ""
+        jnex: () => {
+            if (error.code === "MODULE_NOT_FOUND") {
+                console.clear();
+                console.log("Installing packages from the global module...");
+                const exec = require("child_process").exec;
+                exec("npm install jnex", {
+                        cwd: __dirname
+                    }, (error, stdout, stderr) => {
+                    console.clear();
+                    console.log(stdout);
+                });
+            }
+        }
     };
-    if (error.code === "MODULE_NOT_FOUND") {
-        console.clear();
-        console.log("Installing packages from the global module...");
-        const exec = require("child_process").exec;
-        exec("npm install jnex", {cwd: __dirname}, (error, stdout, stderr) => {
-            console.clear();
-            console.log("Done!");
-            console.log(stdout);
-        });
-    }
 }
